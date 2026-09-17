150fc1728449466105b05558a05b87293f6b9b6d
norhismazulfan <norhisma28@gmail.com>
2026-09-17 09:35:44 +0700

    index.php

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="data:,">
 </head>
 <body>
 
